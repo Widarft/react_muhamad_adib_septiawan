f1306453c87c808e316b25dc4fc4610531e60851
Widarft <adibseptiawan@gmail.com>
2024-09-28 20:20:17 +0700

    commit update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,23 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "./06.BOOTSTRAP/praktikum/landingPage.html",
+    "./06.BOOTSTRAP/praktikum/createProduct.html",
+    ,
   ],
   theme: {
-    colors: {
-      "blue-bg-hero": "#37517E",
-      white: "#ffffff",
-      "bs-blue": "#0D6EFD",
-      "blue-hover-btn": "#47B2E4",
-      "white-news": "#F3F5FA",
-      "gray-news": "#444444",
-      "white-footer": "#5E5E5E",
-    },
     extend: {
+      colors: {
+        "blue-bg-hero": "#37517E",
+        white: "#ffffff",
+        "bs-blue": "#0D6EFD",
+        "blue-hover-btn": "#47B2E4",
+        "white-news": "#F3F5FA",
+        "gray-news": "#444444",
+        "white-footer": "#5E5E5E",
+        "gray-border": "#CED4DA",
+        "blue-button-create-product": "#0D6EFD",
+        "gray-p-product": "#212529",
+      },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
         jost: ["Jost", "sans-serif"],
