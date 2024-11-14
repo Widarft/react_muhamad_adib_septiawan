f0a11a2d3bfe81e948876ae5ddb191c36a1ee96e
Widarft <adibseptiawan@gmail.com>
2024-11-14 17:47:07 +0700

    update commit

--- Greenscape/greenscape/vite.config.js
+++ Greenscape/greenscape/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     includeSource: ["src/**/*.{js,jsx}"],
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
   },
 });
