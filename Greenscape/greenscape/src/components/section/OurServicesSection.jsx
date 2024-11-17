1f943b48b3d81d8828daa721a8a59c2e65de6d89
Widarft <adibseptiawan@gmail.com>
2024-11-17 12:29:34 +0700

    Update Commit Feature Services
    Memisahkan data untuk feature services sehingga jika ingin ada tambahan dapat mengambil dari data tersebut.

--- Greenscape/greenscape/src/components/section/OurServicesSection.jsx
+++ Greenscape/greenscape/src/components/section/OurServicesSection.jsx
@@ -1,29 +1,9 @@
 import React, { useState } from "react";
 import { useNavigationUtils } from "../../hook/navigationUtils";
+import servicesData from "../../data/servicesData";
 
 const OurServicesSection = () => {
   const { navigateTo } = useNavigationUtils();
-
-  const services = [
-    {
-      title: "REFORESTATION",
-      image: "/assets/img/service-reforestation.png",
-      description:
-        "Our reforestation services focus on restoring green spaces using native plants, promoting biodiversity, and enhancing the ecosystem's health.",
-    },
-    {
-      title: "LANDSCAPING",
-      image: "/assets/img/service-landscaping.png",
-      description:
-        "Transform your outdoor space with our expert landscaping services, combining aesthetic design with sustainable practices for a beautiful and eco-friendly environment.",
-    },
-    {
-      title: "MAINTENANCE",
-      image: "/assets/img/service-maintenance.png",
-      description:
-        "Keep your green spaces thriving with our regular maintenance services, ensuring healthy growth and the longevity of your plants.",
-    },
-  ];
 
   // State untuk modal
   const [selectedService, setSelectedService] = useState(null);
@@ -41,7 +21,7 @@ const OurServicesSection = () => {
 
         {/* Kartu Layanan */}
         <div className="flex flex-col md:flex-row items-center justify-center gap-8 pb-20">
-          {services.map((service, index) => (
+          {servicesData.map((service, index) => (
             <div
               key={index}
               onClick={() => setSelectedService(service)}
